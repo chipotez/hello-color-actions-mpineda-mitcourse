4147e8e124b66056eaf6fc47b9c6c4bc5354b00e
Th3Mik3 <mapm.pineda@gmail.com>
2023-07-30 21:23:28 -0600

    Update hello.test.js

--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,6 @@
 const hello = require("./hello");
 describe("My hello", () => {
   test("works", () => {
-    expect(hello.hello()).toEqual("Hola mundo - curso MIt Cap 4 - Miguel Pineda");
+    expect(hello.hello()).toEqual("Hola mundo Miguel Pineda");
   });
 });
